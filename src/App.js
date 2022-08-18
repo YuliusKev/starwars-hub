1632c91fa5f0caa5055ffd588945065317241a15
unknown <juliussiregar14@gmail.com>
2022-08-19 01:11:17 +0700

    Add routing to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import Characters from './components/Characters'
@@ -7,8 +8,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Dashboard /> */}
-      <Characters/>
+      <Routes>
+        <Route path="/" element={ <Dashboard/> } />
+        <Route path="characters" element={ <Characters/> } />
+        {/* <Dashboard /> */}
+        {/* <Characters/> */}
+      </Routes>
     </div>
   );
 }
